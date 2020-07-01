fadcdb5bcdaadcb4343eb73d884cd3a3f500ad73
Haimerl <HAIMEJ@de.softlab.net>
2020-07-01 10:46:33 +0200

    finalize

--- Assets/Scripte/RaceFinish.cs
+++ Assets/Scripte/RaceFinish.cs
@@ -6,20 +6,16 @@ using UnityStandardAssets.Vehicles.Car;
 public class RaceFinish : MonoBehaviour {
 
 	public GameObject MyCar;
-	public GameObject CompleteTrig;
 
 	void OnTriggerEnter () {
 		this.GetComponent<BoxCollider> ().enabled = false;
-		CompleteTrig.SetActive (false);
-        MyCar.SetActive(false);
-		CarController.m_Topspeed = 0.0f;
-		MyCar.GetComponent<CarController> ().enabled = false;
-		//only when not using the waypointbased car
-        //MyCar.GetComponent<CarUserControl> ().enabled = false;
-		MyCar.GetComponent<CarAudio> ().enabled = false;
+        
+		//self
+		//MyCar.GetComponent<CarController> ().enabled = false;
+
+		//AI
         MyCar.GetComponent<CarAIControl>().m_Driving = false;
-        MyCar.GetComponent<CarAIControl>().enabled = false;
-        MyCar.SetActive(true);
+
     }
 
 }
